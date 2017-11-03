a0b6884d86cb59f8370dd9cb02f0c63fbab78718
Charles <crossan007@gmail.com>
2017-11-02 21:11:22 -0400

    service cleanup

--- start-FCCPi.sh
+++ start-FCCPi.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 v4l2-ctl --set-ctrl=exposure_auto=1
-v4l2-ctl --set-ctrl=exposure_absolute=180
-v4l2-ctl --set-ctrl=exposure_absolute=190
 v4l2-ctl --set-ctrl=focus_auto=0
 v4l2-ctl --set-ctrl=focus_absolute=0
 v4l2-ctl --set-ctrl=exposure_absolute=170
